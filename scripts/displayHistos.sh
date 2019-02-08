93c99066757a509c2647558be44f48cd46cdf406
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2019-02-08 14:58:23 +0100

    change path to gcc and ROOT

--- scripts/displayHistos.sh
+++ scripts/displayHistos.sh
@@ -7,8 +7,8 @@ echo $2
 echo $3
 
 source /opt/rh/python27/enable
-source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.08/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+source /cvmfs/sft.cern.ch/lcg/releases/gcc/4.9.3/x86_64-slc6-gcc49-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.06.06-a2c9d/x86_64-slc6-gcc49-opt/bin/thisroot.sh
 python -V
 root-config --version
 
