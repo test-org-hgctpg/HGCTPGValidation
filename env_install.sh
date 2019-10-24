7e62b7f2d7000eddd1516f12cbdd77937369ed12
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2019-10-24 15:23:45 +0200

    Print some useful information

--- env_install.sh
+++ env_install.sh
@@ -4,10 +4,13 @@
 #source /opt/rh/rh-python36/enable
 
 # at LLR, working with sl6 releases
-source /usr/share/Modules/init/bash
-module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles
-module load python/3.6.3
+#source /usr/share/Modules/init/bash
+#module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles
+#module load python/3.6.3
 
 # at LLR, working with sl7 releases
-#module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/
-#module load python/3.6.6
+module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/
+module unload python/3.6.3
+module load python/3.6.6
+uname -a
+python --version
