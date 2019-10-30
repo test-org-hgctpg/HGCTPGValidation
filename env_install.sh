f36b0b0ac55acc668658165a059e93ed4d7072c4
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2019-10-30 17:14:02 +0100

    Set a variable in order to select the working environment only in env_install.sh file

--- env_install.sh
+++ env_install.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# at CERN
-#source /opt/rh/rh-python36/enable
+# Thise variables are get also in displayHistos.sh in order to set the appropriate environment
+# CERN         1
+# LLR, sl6     2
+# LLR, sl7     3
+simu_env=3
+export simu_env
 
-# at LLR, working with sl6 releases
-#source /usr/share/Modules/init/bash
-#module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles
-#module load python/3.6.3
+if [ $simu_env -eq 1 ]
+then
+    # at CERN
+    source /opt/rh/rh-python36/enable
+    echo 'Working at CERN environment'
+elif [ $simu_env -eq 2 ]
+then
+    # at LLR, working with sl6 releases
+    source /usr/share/Modules/init/bash
+    module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles
+    module load python/3.6.3
+    echo 'Working at LLR in sl6 environment'
+else [ $simu_env -eq 3 ]
+    # at LLR, working with sl7 releases
+    module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/
+    module unload python/3.6.3
+    module load python/3.6.6
+    echo 'Working at LLR in sl7 environment'
+fi
 
-# at LLR, working with sl7 releases
-module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/
-module unload python/3.6.3
-module load python/3.6.6
 uname -a
 python --version
--- scripts/displayHistos.sh
+++ scripts/displayHistos.sh
@@ -5,23 +5,33 @@ echo $#
 echo $1
 echo $2
 echo $3
+echo $simu_env
 
-# Config at Cern
-#source /opt/rh/python27/enable
-#source /cvmfs/sft.cern.ch/lcg/releases/gcc/4.9.3/x86_64-slc6-gcc49-opt/setup.sh
-#source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.06.06-a2c9d/x86_64-slc6-gcc49-opt/bin/thisroot.sh
+if [ $simu_env -eq 1 ]
+then
+    # Config at Cern
+    source /opt/rh/python27/enable
+    source /cvmfs/sft.cern.ch/lcg/releases/gcc/4.9.3/x86_64-slc6-gcc49-opt/setup.sh
+    source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.06.06-a2c9d/x86_64-slc6-gcc49-opt/bin/thisroot.sh
+elif [ $simu_env -eq 2 ]
+then
+    # Config at LLR in sl6, switch to python2
+    source /usr/share/Modules/init/bash
+    module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles
+    module swap python/3.6.3 python/2.7.10
+    source /opt/exp_soft/llr/root/v6.06.00-el6-gcc48/etc/init.sh
+else [ $simu_env -eq 3 ]
+    # Config at LLR in sl7, switch to python2
+    #module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/
+    source /usr/share/Modules/init/bash
+    module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles
+    module swap python/3.6.6 python/2.7.10
+    source /opt/exp_soft/llr/root/v6.06.00-el6-gcc48/etc/init.sh
+fi
 
-# Config at LLR in sl6, switch to python2
-source /usr/share/Modules/init/bash
-module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles
-module swap python/3.6.3 python/2.7.10
-source /opt/exp_soft/llr/root/v6.06.00-el6-gcc48/etc/init.sh
-
-# Config at LLR in sl7, switch to python2
-#module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/
-#module swap python/3.6.6 python/2.7.10
-
+echo 'The Python version was changed to '
 python -V
+echo 'The ROOT version is '
 root-config --version
 
 find . -name "out_ref.log" | xargs grep "TimeModule>" > TimingInfo_ref.txt
