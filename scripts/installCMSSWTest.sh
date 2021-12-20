4fa8ff0fafb6b97cf600fc1b7740578fc0f9ea04
ebecheva <emilia.becheva@llr.in2p3.fr>
2021-12-20 18:00:58 +0100

    Variable for the release
    Introduce a variable for the release

--- scripts/installCMSSWTest.sh
+++ scripts/installCMSSWTest.sh
@@ -1,14 +1,17 @@
 
 #!/bin/bash
 
+# installCMSSWTest.sh $RELEASE
+# $1 name of the release
+
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc900
 echo $SCRAM_ARCH
 module purge
-scramv1 p -n CMSSW_12_1_0_pre3_HGCalTPGValidation_test CMSSW_12_1_0_pre3
-cd CMSSW_12_1_0_pre3_HGCalTPGValidation_test/src
+scramv1 p -n $1_HGCalTPGValidation_test $1
+cd $1_HGCalTPGValidation_test/src
 echo $PWD
 eval `scramv1 runtime -sh`
-git cms-merge-topic ebecheva:fix-dev-CMSSW_12_1_0_pre3
-git checkout -b local_main-dev-CMSSW_12_1_0_pre3 ebecheva/fix-dev-CMSSW_12_1_0_pre3
+git cms-merge-topic ebecheva:fix-dev-$1
+git checkout -b local_main-dev-$1 ebecheva/fix-dev-$1
 scram b -j8
