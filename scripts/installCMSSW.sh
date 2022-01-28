a6152977d813ebc0c7c362c19bf8d0659f5c72f0
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-01-28 10:57:17 +0100

    Update installCMSSW.sh
    Get code from hgc-tpg

--- scripts/installCMSSW.sh
+++ scripts/installCMSSW.sh
@@ -17,8 +17,6 @@ scramv1 p -n $1_HGCalTPGValidation_$3 CMSSW $1
 cd $1_HGCalTPGValidation_$3/src
 echo $PWD
 eval `scramv1 runtime -sh`
-#git config --global user.name "Emilia Becheva"
-#git config --global user.email emilia.becheva@llr.in2p3.fr
-git cms-merge-topic ebecheva:$2
-git checkout -b local_$2 ebecheva/$2
+git cms-merge-topic hgc-tpg:$2
+git checkout -b local_$2 hgc-tpg/$2
 scram b -j8
