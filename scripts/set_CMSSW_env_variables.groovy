01657737a565305db29632f53b8f8ebc7349c570
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2025-04-09 14:37:40 +0200

    Set quotation marks in prinln

--- scripts/set_CMSSW_env_variables.groovy
+++ scripts/set_CMSSW_env_variables.groovy
@@ -2,9 +2,9 @@
 
 def run(String JOB_FLAG, String CHANGE_FORK, String BASE_REMOTE) {
     println('In the run function')
-    println(${JOB_FLAG})
-    println(${CHANGE_FORK})
-    println(${BASE_REMOTE})
+    println("${JOB_FLAG}")
+    println("${CHANGE_FORK}")
+    println("${BASE_REMOTE}")
     
     if ( ${JOB_FLAG} == '0' ){
         env.REF_RELEASE = sh(returnStdout: true, script: 'set +x exec >> log_Jenkins; source ./HGCTPGValidation/scripts/extractReleaseName.sh ${CHANGE_TARGET}').trim()
@@ -18,10 +18,10 @@ def run(String JOB_FLAG, String CHANGE_FORK, String BASE_REMOTE) {
         else {
             env.REMOTE = ${BASE_REMOTE}
         }
-            println(env.REF_RELEASE)
-            println(env.SCRAM_ARCH)
-            println(env.TEST_RELEASE)
-            println(env.REMOTE)
+            println("env.REF_RELEASE")
+            println("env.SCRAM_ARCH")
+            println("env.TEST_RELEASE")
+            println("env.REMOTE")
         } 
         else {
             env.REF_BRANCH = sh(returnStdout: true, script: 'set +x exec >> log_Jenkins; module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/; module purge; module load python/3.9.9; python ./HGCTPGValidation/scripts/get_cmsswRefBranch.py').trim()
@@ -33,14 +33,14 @@ def run(String JOB_FLAG, String CHANGE_FORK, String BASE_REMOTE) {
             env.REMOTE = env.BASE_REMOTE
             env.TEST_RELEASE = env.REF_RELEASE
                                 
-            println(env.REF_BRANCH)
-            println(env.REF_RELEASE)
-            println(env.TEST_RELEASE)
-            println(env.SCRAM_ARCH)
-            println(env.BASE_REMOTE)
-            println(env.CHANGE_BRANCH)
-            println(env.CHANGE_TARGET)
-            println(env.REMOTE)
+            println("env.REF_BRANCH")
+            println("env.REF_RELEASE")
+            println("env.TEST_RELEASE")
+            println("env.SCRAM_ARCH")
+            println("env.BASE_REMOTE")
+            println("env.CHANGE_BRANCH")
+            println("env.CHANGE_TARGET")
+            println("env.REMOTE")
         }
 }
 
