5429b23d367daf5724aa78841f5e50649bbd76f7
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2025-10-07 19:35:55 +0200

    Add some prints, comments

--- scripts/check_scripts.sh
+++ scripts/check_scripts.sh
@@ -3,11 +3,15 @@
 # $2 script file name
 # Usage check_scripts.sh $? {script_file}_USER.py
 
+echo "status = " $1
+echo "script name = " $2
+
 # If the script file exists and is not empty => OK
-# If this not the case the scrip returns status 1
+# If this not the case the script returns the status 1
 if [ -s $2 ] ; then
     echo "The script " $2 "was created.";
 else
     echo "     "
-    echo "!!!! cmsDriver failed to execute! The script " $2 "has not been created!"; exit 1;
+    echo "!!!! cmsDriver failed to execute! The script " $2 "has not been created!";
+    exit 1;
 fi
