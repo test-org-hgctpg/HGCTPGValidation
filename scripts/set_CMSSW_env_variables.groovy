e42acd17f6b80829953f4ab55f15b958fb3bd03d
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2026-02-18 12:35:39 +0100

    Rearrange debug messages

--- scripts/set_CMSSW_env_variables.groovy
+++ scripts/set_CMSSW_env_variables.groovy
@@ -7,9 +7,10 @@
 
 def run(String JOB_FLAG, String CHANGE_FORK, String CHANGE_TARGET, String BASE_REMOTE) {
     println('Input variables in the run function')
-    println("${JOB_FLAG}")
-    println("${CHANGE_FORK}")
-    println("${BASE_REMOTE}")
+    println("JOB_FLAG=${JOB_FLAG}")
+    println("CHANGE_FORK=${CHANGE_FORK}")
+    println("CHANGE_TARGET=${CHANGE_TARGET}")
+    println("BASE_REMOTE=${BASE_REMOTE}")
     
     if ( JOB_FLAG == '0' ){
         env.REF_RELEASE = sh(returnStdout: true, script: 'set +x; source ./HGCTPGValidation/scripts/extractReleaseName.sh ${CHANGE_TARGET}').trim()
@@ -23,30 +24,27 @@ def run(String JOB_FLAG, String CHANGE_FORK, String CHANGE_TARGET, String BASE_R
         else {
             env.REMOTE = BASE_REMOTE
         }
-            println(env.REF_RELEASE)
-            println(env.SCRAM_ARCH)
-            println(env.TEST_RELEASE)
-            println(env.REMOTE)
     }
     else {
-            env.REF_BRANCH = sh(returnStdout: true, script: 'set +x; module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el9/; module purge; module load python/latest; python ./HGCTPGValidation/scripts/get_cmsswRefBranch.py').trim()
-            env.REF_RELEASE = sh(returnStdout: true, script: 'set +x; source ./HGCTPGValidation/scripts/extractReleaseName.sh ${REF_BRANCH}').trim()
-            env.SCRAM_ARCH = sh(returnStdout: true, script: 'set +x; source ./HGCTPGValidation/scripts/getScramArch.sh ${REF_RELEASE}').trim()
-            env.BASE_REMOTE = sh(returnStdout: true, script: 'set +x; module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el9/; module purge; module load python/latest; python ./HGCTPGValidation/scripts/get_remoteParam.py').trim()
-            env.CHANGE_BRANCH = env.REF_BRANCH
-            env.CHANGE_TARGET = env.REF_BRANCH
-            env.REMOTE = env.BASE_REMOTE
-            env.TEST_RELEASE = env.REF_RELEASE
-                                
-            println(env.REF_BRANCH)
-            println(env.REF_RELEASE)
-            println(env.TEST_RELEASE)
-            println(env.SCRAM_ARCH)
-            println(env.BASE_REMOTE)
-            println(env.CHANGE_BRANCH)
-            println(env.CHANGE_TARGET)
-            println(env.REMOTE)
+        env.REF_BRANCH = sh(returnStdout: true, script: 'set +x; module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el9/; module purge; module load python/latest; python ./HGCTPGValidation/scripts/get_cmsswRefBranch.py').trim()
+        env.REF_RELEASE = sh(returnStdout: true, script: 'set +x; source ./HGCTPGValidation/scripts/extractReleaseName.sh ${REF_BRANCH}').trim()
+        env.SCRAM_ARCH = sh(returnStdout: true, script: 'set +x; source ./HGCTPGValidation/scripts/getScramArch.sh ${REF_RELEASE}').trim()
+        env.BASE_REMOTE = sh(returnStdout: true, script: 'set +x; module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el9/; module purge; module load python/latest; python ./HGCTPGValidation/scripts/get_remoteParam.py').trim()
+        env.CHANGE_BRANCH = env.REF_BRANCH
+        env.CHANGE_TARGET = env.REF_BRANCH
+        env.REMOTE = env.BASE_REMOTE
+        env.TEST_RELEASE = env.REF_RELEASE
+        
     }
+    
+    println("REF_BRANCH=${REF_BRANCH}")
+    println("REF_RELEASE=${REF_RELEASE}")
+    println("TEST_RELEASE=${TEST_RELEASE}")
+    println("SCRAM_ARCH=${SCRAM_ARCH}")
+    println("BASE_REMOTE=${BASE_REMOTE}")
+    println("CHANGE_BRANCH=${CHANGE_BRANCH}")
+    println("CHANGE_TARGET=${CHANGE_TARGET}")
+    println("REMOTE=${REMOTE}")
 }
 
 return this
