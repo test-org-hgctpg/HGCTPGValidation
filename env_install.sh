9942be7c044a48db2604fbb80dca96f1535fb790
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2026-02-27 17:47:50 +0100

    Add ROOT libraries and appropriate gcc version

--- env_install.sh
+++ env_install.sh
@@ -21,6 +21,8 @@ then
     module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el9/
     module purge
     module load python/latest
+    module load compilers/gcc/9.x.x
+    source /opt/exp_soft/llr/root/vv6.20.06-el7-gcc9-py37/etc/init.sh
     #echo 'Working at LLR in sl9 environment'
 fi
 
