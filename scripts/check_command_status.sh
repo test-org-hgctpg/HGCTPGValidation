4a2fb01e0309e059b432bfa32c24b9b238bc5675
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2026-04-29 17:36:26 +0200

    Add new scripts to manage new logs

---
+++ scripts/check_command_status.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# $1 status variable
+# $2 stage name
+
+# If the script displayHistos.py failed, the pipeline stops
+if [ $1 -gt 0 ];
+then
+    echo ' Error in stage ' $2 ', with status=' $1
+    cat ../../../out_err >&2
+    exit $1
+else
+    echo ' The stage ' $2 ' completed successufully!'
+fi
