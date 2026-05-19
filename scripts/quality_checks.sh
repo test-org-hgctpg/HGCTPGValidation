47a54719e896697bdab5df67824bfb865c8c4c67
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2026-05-19 21:08:18 +0200

    Redirect only to stderr

--- scripts/quality_checks.sh
+++ scripts/quality_checks.sh
@@ -19,7 +19,7 @@ scram build code-checks
 scram build code-format
 GIT_STATUS=`git status --porcelain`
 if [ ! -z "$GIT_STATUS" ]; then
-    echo "Code-checks or code-format failed." 1>&2
+    echo "Code-checks or code-format failed." >&2
     exit 1;
 fi
 echo '    '
